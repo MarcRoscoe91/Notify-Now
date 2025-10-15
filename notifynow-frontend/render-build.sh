aac37b26caca23a198b06c8247bab2086880fd0e
MarcRoscoe91 <marc_roscoe@hotmail.com>
2025-10-15 04:53:29 +0100

    Update render-build.sh

--- notifynow-frontend/render-build.sh
+++ notifynow-frontend/render-build.sh
@@ -1,9 +1,3 @@
-diff --git a/notifynow-frontend/render-build.sh b/notifynow-frontend/render-build.sh
-new file mode 100755
-index 0000000000000000000000000000000000000000..f6ef88ab6fe3b7f418531de343e4dcbc8e86655a
---- /dev/null
-+++ b/notifynow-frontend/render-build.sh
-@@ -0,0 +1,11 @@
 +#!/usr/bin/env bash
 +set -euo pipefail
 +cd "$(dirname "$0")"
