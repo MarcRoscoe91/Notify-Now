e05dc13c68b7f597a38000a14488ada307366c38
MarcRoscoe91 <marc_roscoe@hotmail.com>
2025-10-15 04:50:17 +0100

    Update config.js

--- notifynow-frontend/js/config.js
+++ notifynow-frontend/js/config.js
@@ -1,2 +1,8 @@
-// This file is overwritten by render-build.sh during deployments.
-window.__NOTIFY_NOW_API__ = '/api';
+diff --git a/notifynow-frontend/js/config.js b/notifynow-frontend/js/config.js
+new file mode 100644
+index 0000000000000000000000000000000000000000..6fcc0a890d4ca29cfdfdc1739c550ab6067f951d
+--- /dev/null
++++ b/notifynow-frontend/js/config.js
+@@ -0,0 +1,2 @@
++// This file is overwritten by render-build.sh during deployments.
++window.__NOTIFY_NOW_API__ = '/api';
